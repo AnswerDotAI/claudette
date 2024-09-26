28478da793e40d03d5ebf4fb81a7abd1a047fbc5
Jeremy Howard <github@jhoward.fastmail.fm>
2024-09-26 10:00:24 +1000

    fixes #30

--- claudette/core.py
+++ claudette/core.py
@@ -294,7 +294,7 @@ def mk_msg(content, # A string, list, or dict containing the contents of the mes
     if isinstance(content, abc.Mapping): content=content.get('content', content)
     if not isinstance(content, list): content=[content]
     content = [_mk_content(o, cache if islast else False) for islast,o in loop_last(content)] if content else '.'
-    return dict(role=role, content=content, **kw)
+    return dict2obj(dict(role=role, content=content, **kw))
 
 # %% ../00_core.ipynb
 models_aws = ('anthropic.claude-3-haiku-20240307-v1:0', 'anthropic.claude-3-sonnet-20240229-v1:0',
