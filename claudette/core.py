153b3a8e2e150e362bc192c6c7a0eaa8e384cb5d
Jeremy Howard <github@jhoward.fastmail.fm>
2025-11-26 13:48:29 +1000

    opus

--- claudette/core.py
+++ claudette/core.py
@@ -38,9 +38,10 @@ empty = inspect.Parameter.empty
 # %% ../00_core.ipynb
 model_types = {
     # Anthropic
-    'claude-opus-4-1-20250805': 'opus',
+    'claude-opus-4-5': 'opus',
     'claude-sonnet-4-5': 'sonnet',
     'claude-haiku-4-5': 'haiku',
+    'claude-opus-4-1-20250805': 'opus-4-1',
     'claude-opus-4-20250514': 'opus-4',
     'claude-3-opus-20240229': 'opus-3',
     'claude-sonnet-4-20250514': 'sonnet-4',
@@ -65,7 +66,7 @@ model_types = {
 all_models = list(model_types)
 
 # %% ../00_core.ipynb
-models = all_models[:8]
+models = all_models[:10]
 
 # %% ../00_core.ipynb
 models_aws = [
@@ -96,8 +97,9 @@ has_streaming_models = set(all_models)
 has_system_prompt_models = set(all_models)
 has_temperature_models = set(all_models)
 has_extended_thinking_models = {
-    'claude-opus-4-1-20250805', 'claude-opus-4-20250514', 'claude-sonnet-4-20250514', 'claude-3-7-sonnet-20250219',
-    'sonnet-4-5', 'haiku-4-5'
+    'claude-opus-4-5', 'claude-opus-4-1-20250805', 'claude-opus-4-20250514',
+    'claude-sonnet-4-20250514', 'claude-3-7-sonnet-20250219', 'sonnet-4-5',
+    'haiku-4-5'
 }
 
 # %% ../00_core.ipynb
@@ -286,7 +288,7 @@ def __call__(self:Client,
 
 # %% ../00_core.ipynb
 pricing = {  # model type: $ / million tokens (input, output, cache write, cache read)
-    'opus': (15, 75, 18.75, 1.5),
+    'opus': (5, 25, 6.25, 0.5),
     'sonnet': (3, 15, 3.75, 0.3),
     'haiku': (1, 5, 1.25, 0.1),
     'haiku-3': (0.25, 1.25, 0.3, 0.03),
